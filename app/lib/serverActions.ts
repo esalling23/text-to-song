19a860ca303fbab4bff92500701411a7c046f2b3
Eron Salling <esalling23@gmail.com>
2024-04-19 13:34:54 -0400

    Renames server actions file, adds update function

---
+++ app/lib/serverActions.ts
@@ -0,0 +1,29 @@
+'use server';
+import prisma from "../../prisma";
+import { PlayerData } from "./types";
+
+export async function updatePlayer(data: PlayerData) {
+  
+  const player = await prisma.player.update({
+		where: {
+			id: data.id
+		},
+		data: {
+			displayName: data.displayName
+		}
+	});
+
+	console.log(player);
+
+  return { success: true, data: player };
+}
+
+export async function updateGameData(id: string) {
+	const game = await prisma.game.findUnique({ where: { id } })
+
+	if (!game) {
+		return { success: false, error: 'No game found' }
+	}
+
+	return { success: true, data: game }
+}
