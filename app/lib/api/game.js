6530a5725ca4e91c5137d8d2a1e79ce84e0a8f57
Eron Salling <esalling23@gmail.com>
2024-12-27 21:10:29 -0500

    Adds various api functions

--- app/lib/api/game.js
+++ app/lib/api/game.js
@@ -1,12 +1,18 @@
 import { fetcher, poster } from "@/lib/fetchHelpers"
 
-export const createGame = (socketId) => poster('/game/create', { socketId })
+export const createGame = (socketId) => poster('/api/game', { socketId })
 
-export const killGame = () => {
-
-}
-
-export const joinGame = (socketId, gameCode) => poster('/game/join', {
-	socketId, 
-	gameCode
+export const killGame = (gameId, socketId) => fetcher(`/api/game/${gameId}`, {
+	method: 'delete',
+	data: { socketId }
 })
+
+export const getGame = (gameId) => fetcher(`/api/game/${gameId}`)
+
+export const joinGame = (socketId, gameCode, playerId) => poster('/api/game/join', {
+	socketId, 
+	gameCode,
+	playerId
+})
+
+export const startGame = (gameId) => poster(`/api/game/${gameId}/start`, {})
