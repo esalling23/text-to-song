f60c6aed6e4a0ff858f6c4aa1b53cbcdb23e82a4
Eron Salling <esalling23@gmail.com>
2024-12-27 21:10:44 -0500

    Adds/updates typescript types

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -16,11 +16,47 @@ export interface PlayerData {
 	id: string;
 	displayName: string;
 	gameCode?: string;
+	totalScore: number;
 }
 
-export interface RoomData {
+export interface GuessData {
+	id: string;
+	title: string;
+	artist: string;
+	playerId: string;
+	roundId: string;
+	score: number;
+	isCorrect: boolean;
+}
+
+export interface GameUpdateData {
+	game: GameData;
+	players: Array<PlayerData>
+}
+
+export interface GameGuessData {
+	roundGuesses: Array<GuessData>
+}
+
+export interface RoundData {
+	id: string;
+	song: {
+		title: string;
+		artist: {
+			name: string;
+		};
+		lyricBlocks: Array<string>;
+	};
+	blockIndex: number;
+}
+
+export interface GameData {
 	id: string;
 	gameCode: string;
 	groupSocketId: string;
-	players: Array<string> | undefined;
+	players: Array<string>;
+	rounds: Array<RoundData>;
+	isActive: boolean;
+	isStarted: boolean;
+	isCompleted: boolean;
 }
