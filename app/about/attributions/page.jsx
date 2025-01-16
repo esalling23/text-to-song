d852c0a16bc4092861bd645ac06681d39ab38ddd
Eron Salling <esalling23@gmail.com>
2025-01-16 11:57:29 -0500

    Adds simple about/attributions pages

---
+++ app/about/attributions/page.jsx
@@ -0,0 +1,11 @@
+
+const AttributionsPage = () => {
+
+	return (<>
+		<a href="https://www.vecteezy.com/free-vector/music">Music Vectors by Vecteezy</a>
+	
+		<a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by kmg design - Flaticon</a>
+	</>)
+}
+
+export default AttributionsPage
---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+	return (
+		<>
+			<h2>About the Project</h2>
+			<p>This project was inspired by a trivia night at The Goldenrod Country Inn in Massachusetts.</p>
+			<p>Built with NextJS, Prisma, Socket.io, and TailwindCSS</p>
+		</>
+	)
+}
